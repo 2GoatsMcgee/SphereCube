3545b533fc48b26a57b275ea5393958608d5ad3d
Nehemyah <nehemyah.a.green@gmail.com>
2023-01-19 16:37:12 -0600

    Initial Commit

---
+++ SphereCube/SphereCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereCubeApp.swift
+//  SphereCube
+//
+//  Created by IIT PHYS 440 on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SphereCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
